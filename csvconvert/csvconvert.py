cbb24767b0a29874f9a789c130d4ce57024988a3
T L <TLCrow@outlook.com>
2018-05-14 17:07:56 +0800

    txt transfrom list

---
+++ csvconvert/csvconvert.py
@@ -0,0 +1,21 @@
+import os
+import re
+
+map = {'1':'Negative','2':'Positive'}
+filename=r'C:\Users\TLCrow\Desktop\1\list.txt'
+file = open(filename,'r')
+list=[]
+for line in file.readlines():
+    tmp = line.split(',')
+    kfb = tmp[0].strip('\n')
+    kfb = os.path.basename(kfb)
+#    db=re.sub('kfb','db',kfb)
+    db=kfb.replace('kfb','db')
+    label = tmp[1].strip('\n')
+    list.append(kfb+':0|'+map[label]+'|'+db)
+    print(kfb+':0|'+map[label]+'|'+db)
+#outpath=r'C:\Users\TLCrow\Desktop\1\data.list'
+outpath='data.list'
+with open(outpath,'w') as f:
+    for lis in list:
+        f.writelines(lis+'\n')
